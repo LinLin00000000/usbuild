b52a3be037977ea3d0943b2630ef82c7aea3321a
Lin <i@linlin.me>
2023-12-20 12:57:49 +0800

    📝 docs: 增加 enableLocalFileRequireInDev 参数的详细描述

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ import babel from '@babel/core'
  * @param {number} [options.port=7100] - 服务器端口，默认为 7100。
  * @param {boolean} [options.autoReload=true] - 是否自动重载，默认为 true。
  * @param {number} [options.autoReloadDelay=1000] - 自动重载延迟时间（毫秒），默认为 1000。
- * @param {boolean} [options.enableLocalFileRequireInDev=false] - 在开发模式下是否启用加载本地文件来绕过 CSP，默认为 false。
+ * @param {boolean} [options.enableLocalFileRequireInDev=false] - 在开发模式下是否启用加载本地文件来绕过 CSP，默认为 false。如果启用该功能，需在浏览器扩展中同时设置油猴允许访问本地文件。
  * @returns {Promise<any>} 返回一个永不会产生结果的 Promise，以阻塞程序往下运行
  */
 export async function build(
